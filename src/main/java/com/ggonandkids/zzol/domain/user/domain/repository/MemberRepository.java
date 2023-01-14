56f0cf1b6433004badabb1a431c3fd0010746021
jiseok419 <pyosik0419@gmail.com>
2023-01-14 19:54:48 +0900

    :sparkles: :: MemberRepository 추가

---
+++ src/main/java/com/ggonandkids/zzol/domain/user/domain/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.ggonandkids.zzol.domain.user.domain.repository;
+
+import com.ggonandkids.zzol.domain.user.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    boolean existsById(String id);
+    boolean existsByIdAndPassword(String id, String password);
+    Member findById(String id);
+    Optional<Member> findByIdAndPassword(String id, String password);
+}
