f0884ea786d9eddc8dbe674e2d6d448cfe01d282
jiseok419 <pyosik0419@gmail.com>
2023-01-14 19:44:08 +0900

    :tada: :: 기초셋팅

---
+++ src/main/java/com/ggonandkids/zzol/ZzolApplication.java
@@ -0,0 +1,13 @@
+package com.ggonandkids.zzol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggonandkids/zzol/ZzolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggonandkids.zzol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
