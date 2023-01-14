647305d3b56a71a4bb2566c5ad84946db0af75fc
jiseok419 <pyosik0419@gmail.com>
2023-01-14 19:58:55 +0900

    :recycle: :: 불필요 코드 삭제

--- src/main/java/com/ggonandkids/zzol/domain/user/presentation/dto/request/MemberRequestDto.java
+++ src/main/java/com/ggonandkids/zzol/domain/user/presentation/dto/request/MemberRequestDto.java
@@ -1,16 +1,14 @@
 package com.ggonandkids.zzol.domain.user.presentation.dto.request;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor @AllArgsConstructor
 public class MemberRequestDto {
     private String id;
     private String password;
 
-    @Builder
-    public MemberRequestDto(String id, String password) {
-        this.id = id;
-        this.password = password;
-    }
+
 }
